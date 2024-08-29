dce4b037733e0f9f95da363ba8e8bd0f3383e4f1
LanceGuy5 <lanceforresth@gmail.com>
2024-08-29 09:53:28 -0400

    big commit

--- CudeTest1/structures/VectorGPU.cu
+++ CudeTest1/structures/VectorGPU.cu
@@ -1,4 +1,95 @@
 
-#include "./headers/VectorGPU.cuh"
+#include "VectorGPU.cuh"
+// #include <cuda_runtime.h>
+using namespace gpu_std;
 
 // TODO define method headers (i.e. VectorGPU<T>::method_name, use generic template)
+
+template <typename T>
+VectorGPU<T>::VectorGPU() {
+  // TODO implement constructor
+}
+
+template <typename T>
+VectorGPU<T>::~VectorGPU() {
+  // TODO implement destructor
+}
+
+// This is where stuff starts to get buggy, we need to make sure this actually works LOL
+template <typename T>
+__host__
+boolean VectorGPU<T>::operator=(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+// same for device
+template <typename T>
+__device__
+boolean VectorGPU<T>::operator=(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+template <typename T>
+__host__
+void VectorGPU<T>::operator+(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+// same for device
+template <typename T>
+__device__
+void VectorGPU<T>::operator+(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+template <typename T>
+__host__
+void VectorGPU<T>::operator-(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+// same for device
+template <typename T>
+__device__
+void VectorGPU<T>::operator-(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+template <typename T>
+__host__
+void VectorGPU<T>::operator*(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+// same for device
+template <typename T>
+__device__
+void VectorGPU<T>::operator*(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+template <typename T>
+__host__
+void VectorGPU<T>::operator/(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+// same for device
+template <typename T>
+__device__
+void VectorGPU<T>::operator/(const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+template <typename T>
+__host__
+T VectorGPU<T>::operator[](const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
+
+// same for device
+template <typename T>
+__device__
+T VectorGPU<T>::operator[](const VectorGPU<T>& other) {
+  // TODO implement copy constructor
+}
